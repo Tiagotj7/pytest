ffa78bcbddbfbfcea623197627571f536f3919d2
Tiagotj7 <tiagowork07@gmail.com>
2024-03-21 10:31:29 -0300

    Calc python

---
+++ index.py
@@ -0,0 +1,23 @@
+#Teste de calculo básico em Python!!!
+
+#Variáveis
+
+x=5
+y=7
+z=8
+
+#Expressões
+
+t= x+y+z
+
+
+w= x*y*z
+
+
+m= x+y/z
+
+#Resultado
+
+print("Resultado de t:",t)
+print("Resultado de w:",w)
+print("Resultado de m:",m)
